178d0a7e602656df8853ae418f56b63d1ba4cbe1
David J Ruyle <djr.taureau@gmail.com>
2017-10-11 13:38:58 -0500

    assignment-5-delete-button completed

--- app.js
+++ app.js
@@ -13,18 +13,36 @@ function onReady() {
         let checkbox = document.createElement('input');
         //set the input's type to checkbox
         checkbox.type = "checkbox";
+        //create the input button for delete
+        let deletebutton = document.createElement('button');
+       
+        //add delete to button
+        deletebutton.value = "Remove";
+        deletebutton.innerHTML = "X";
+        //set onClick
+ 
 
         //set the title
         newLi.textContent = title;
+        //set the id with the same text as the title
+        newLi.id = title;
         //attach the checkbox to the li
         newLi.appendChild(checkbox);
+        //attach the delete button
+        newLi.appendChild(deletebutton);
         //attach the li to the ul
         toDoList.appendChild(newLi);
         //empty the input for the user
         newToDoText.value = '';
+
+       //event listener for removing task item from to do list
+       deletebutton.addEventListener ("click", function() {
+            this.closest('li').remove();
+         });
+
     });
     
-    }
+ }
 
 
 window.onload = function() {
