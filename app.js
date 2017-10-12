46ebdff6f3f7e697f534a1ad85130ab2766e6971
David J Ruyle <djr.taureau@gmail.com>
2017-10-12 16:32:26 -0500

    completed assigment 8 DOM Scripting Part 2

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 function onReady() {
     const toDos = [];
     const addToDoForm = document.getElementById('addToDoForm');
-  
+
     function createNewToDo() {
         const newToDoText = document.getElementById('newToDoText');
+        
         if(!newToDoText) { return }
         toDos.push({
             title: newToDoText.value,
@@ -14,10 +15,15 @@ function onReady() {
         renderTheUi(toDos);
     }
 
+    function deleteToDo () {
+         this.closest('li').remove();
+    }
+
     addToDoForm.addEventListener('submit', event => {
         event.preventDefault();
         createNewToDo();
     });
+
 
     function renderTheUi(toDos) {
         const toDoList = document.getElementById('toDoList');
@@ -26,11 +32,19 @@ function onReady() {
         toDos.forEach(function(toDo){
             const newLi = document.createElement('li');
             const checkbox = document.createElement('input');
+            const deletebutton = document.createElement('button');
             checkbox.type= 'checkbox';
+            deletebutton.value = 'Remove';
+            deletebutton.innerHTML = 'X';
             newLi.textContent = toDo.title;
+            newLi.id = toDo.title;
             toDoList.appendChild(newLi);
             newLi.appendChild(checkbox);
+            newLi.appendChild(deletebutton);
+            newLi.onclick = deleteToDo;
         });
+
+
     }
  }
 
